5f0016e5df43cc1dee1ba2800e00bdad08b106aa
jeffrey k eliasen <jeff@jke.net>
2020-03-16 16:54:40 -1000

    clearer sample code and full usage page

--- sample/main.py
+++ sample/main.py
@@ -1,3 +1,8 @@
-from cloud_functions_dispatch import execute as execute_dispatched  # noqa
+# this file is the entrypoint for dispatched functions; the only purpose is to
+# provide a function (`execute_dispatched`) for cloud functions to call,
+# and to ensure all the functions are imported and registered (any function
+# that is not imported at startup will not be visible to
+# `cloud_functions_dispatch`)
 
+from cloud_functions_dispatch import execute as execute_dispatched  # noqa
 import functions  # noqa
---
+++ sample/my_module.py
@@ -0,0 +1,7 @@
+from functions import my_func
+
+
+def call_dispatched_function():
+    a = 1
+    b = 2
+    my_func(a, b)
